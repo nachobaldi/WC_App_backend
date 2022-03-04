043566557c9a78b4d0972f0c5b49707d1350b243
Ignacio Baldi <82165568+nachobaldi@users.noreply.github.com>
2022-03-04 10:47:12 +0200

    removing all unnecessary statements

--- API/Controllers/ProjectsController.cs
+++ API/Controllers/ProjectsController.cs
@@ -2,10 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using DbUtils;
 using Entities;
-using System.Web.Http.Results;
+
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 using DbUtils;
 using Entities;
 
